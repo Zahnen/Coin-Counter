5bba538984a74b89c277145edc6c346d30c8cde4
Zahnen Garner <zahnen@gmail.com>
2021-02-01 14:43:14 -0800

    instatiate coinCount array within recursive function
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- src/js/placeholder.js
+++ src/js/placeholder.js
@@ -1,3 +1,43 @@
+coinValues = [25, 10, 5, 1];
+// coinCount = [];
+
+const coinCounter = (moneyToCount, coinCount) => {
+  // let coinCount = coinCount || [];
+  if (!coinCount) {
+    coinCount = [];
+  }
+  if (moneyToCount <= 0) {
+    return coinCount;
+  } else {
+    moduloValue = moneyToCount % coinValues[0];
+    coinCount.push((moneyToCount - moduloValue) / coinValues[0]);
+    moneyToCount = moneyToCount - coinCount[coinCount.length-1] * coinValues[0];
+    coinValues.shift();
+    return coinCounter(moneyToCount, coinCount);
+  }
+}
+
+// if (coinCount) {
+//   continue;
+// } else coinCount = [];
+
+console.log(coinCounter(499));
+
+// REFACTOR
+// Analysis:
+// 1. We always get the same output; it only works once - NOT DONE
+// 2. Always returns something - DONE
+// 3. Cannot rely on external variables - NOT DONE
+// 4. Cannot rely on external state - ? don't think we have state
+// 5. No side effects - ? not sure
+
+// Fixes:
+// 1. How do we use index values in a recursive function? 
+// 2. In the simple example of the doubledArray method we have the numArray being doubled into this new array. Can we combine .map() and recursion
+// to factor this code. That way, we would never need to instantiate/convert coinValues/coinCount
+
+// --------
+// Setup
 // Take amount of money (4.99)
 // FYI: Base case == 0 (?)
 // Key value pair of coin increments 
@@ -19,38 +59,9 @@
 
 // 19 Quarters, 2 dimes, 0 nickels, 4 pennies
 
-coinValues = [25, 10, 5, 1];
-coinCount = [];
-const coinCounter = (moneyToCount) => {
-  if (moneyToCount <= 0) {
-    return coinCount;
-  } else {
-    moduloValue = moneyToCount % coinValues[0];
-    coinCount.push((moneyToCount - moduloValue) / coinValues[0]);
-    moneyToCount = moneyToCount - coinCount[coinCount.length-1] * coinValues[0];
-    coinValues.shift();
-    return coinCounter(moneyToCount);
-  }
-}
 
-console.log(coinCounter(499));
+// Is using i pure? How do we instantiate a variable one time, when the recursive call 
+// We're zeroing out coinValues -> using index(i)
+// How could we get the closure to collapse in such a way that we don't need to append each iterations coinCount
 
-// const incrementCounter = (counter) => {
-//   // counter is 3 or more
-//   if (counter >= 3) {
-//     return counter;
-//   } 
-//   // counter is less than 3
-//   else {
-//     console.log(counter);
-//     return incrementCounter(counter + 1);
-//   }
-// }
-
-// const recurseReverse = (string) => {
-//   if (string === "") {
-//     return "";
-//   } else {
-//     return recurseReverse(string.substring(1)) + string[0];
-//   }
-// }
+// Define coincount at first level inside of function, but you declare let array = array, you'll pass it or 
