4c418ce8048ad966668a76a4e41f86dd265d4d53
Zahnen Garner <zahnen@gmail.com>
2021-02-01 14:51:09 -0800

    make function PURE
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- src/js/placeholder.js
+++ src/js/placeholder.js
@@ -1,11 +1,10 @@
-coinValues = [25, 10, 5, 1];
-// coinCount = [];
-
-const coinCounter = (moneyToCount, coinCount) => {
-  // let coinCount = coinCount || [];
+const coinCounter = (moneyToCount, coinCount, coinValues) => {
   if (!coinCount) {
     coinCount = [];
   }
+  if (!coinValues) {
+    coinValues = [25, 10, 5, 1];
+  } 
   if (moneyToCount <= 0) {
     return coinCount;
   } else {
@@ -13,23 +12,22 @@ const coinCounter = (moneyToCount, coinCount) => {
     coinCount.push((moneyToCount - moduloValue) / coinValues[0]);
     moneyToCount = moneyToCount - coinCount[coinCount.length-1] * coinValues[0];
     coinValues.shift();
-    return coinCounter(moneyToCount, coinCount);
+    return coinCounter(moneyToCount, coinCount, coinValues);
   }
 }
 
-// if (coinCount) {
-//   continue;
-// } else coinCount = [];
-
 console.log(coinCounter(499));
+console.log(coinCounter(514));
+console.log(coinCounter(625));
+console.log(coinCounter(789));
 
 // REFACTOR
 // Analysis:
-// 1. We always get the same output; it only works once - NOT DONE
+// 1. We always get the same output - DONE
 // 2. Always returns something - DONE
-// 3. Cannot rely on external variables - NOT DONE
-// 4. Cannot rely on external state - ? don't think we have state
-// 5. No side effects - ? not sure
+// 3. Cannot rely on external variables - DONE
+// 4. Cannot rely on external state - DONE - not 100% sure
+// 5. No side effects - DONE
 
 // Fixes:
 // 1. How do we use index values in a recursive function? 
@@ -65,3 +63,4 @@ console.log(coinCounter(499));
 // How could we get the closure to collapse in such a way that we don't need to append each iterations coinCount
 
 // Define coincount at first level inside of function, but you declare let array = array, you'll pass it or 
+// let coinCount = coinCount || [];
