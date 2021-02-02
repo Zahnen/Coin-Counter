fb435488f21a4d54e8f7eacd8a0061ee036a4fc1
Zahnen Garner <zahnen@gmail.com>
2021-02-01 16:14:08 -0800

    refactor function
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- src/js/placeholder.js
+++ src/js/placeholder.js
@@ -1,18 +1,11 @@
-const coinCounter = (moneyToCount, coinCount, coinValues) => {
-  if (!coinCount) {
-    coinCount = [];
-  }
-  if (!coinValues) {
-    coinValues = [25, 10, 5, 1];
-  } 
+const coinCounter = (moneyToCount, coinCount = [], coinValues = [25,10,5,1]) => {
   if (moneyToCount <= 0) {
     return coinCount;
   } else {
     moduloValue = moneyToCount % coinValues[0];
-    coinCount.push((moneyToCount - moduloValue) / coinValues[0]);
-    moneyToCount = moneyToCount - coinCount[coinCount.length-1] * coinValues[0];
-    coinValues.shift();
-    return coinCounter(moneyToCount, coinCount, coinValues);
+    newCoinCount = coinCount.concat(Math.floor(moneyToCount / coinValues[0]));
+    newCoinValues = coinValues.slice(1);
+    return coinCounter(moduloValue, newCoinCount, newCoinValues);
   }
 }
 
@@ -21,18 +14,48 @@ console.log(coinCounter(514));
 console.log(coinCounter(625));
 console.log(coinCounter(789));
 
-// REFACTOR
-// Analysis:
-// 1. We always get the same output - DONE
-// 2. Always returns something - DONE
-// 3. Cannot rely on external variables - DONE
-// 4. Cannot rely on external state - DONE - not 100% sure
-// 5. No side effects - DONE
+// Travis Refactor Suggestions
+// a) Utilize default parameters, defaults, in the definition of function. Arguments that you might pass in, and might not you can do coinCount = []
+// b) floor() is a factor because it rounds down: (moneyToCount - moduloValue)
+// c) line 7 moneyToCount - coinCount[coinCount.length-1] * coinValues[0] simply evaluates to modulovalue found on line 5
+// d) line 8 is mutating, and it's bad. Don't do side effects... but updating the DOM is a side effect (because it's not a return value).
+// e) shift (mutating) refactor: new variable equal to coinValues[1]
+// f) push (mutating) refactor: spread operator (tonights HW), concat 
+// f continued) spread operator suggestion elippses ... 
+// equals "take all the values inside arr2 and spread them out"
+// let arr1 = ['a','b','c'];
+// let arr2 = arr1.slice(1);
+// let arr3 = arr2.concat('d');
+
+// function asdf(a, b, c);
+// asdf(...arr1); // works
+// asdf(arr1) // doesnt work
+
+// make a new array, and tack on another elemtn
+// let arr3 = [...arr2, 'b'];
+
+// altenratively, use concat
+// let arr3 = arr2.concat('d');
+
+// g) refactor return value into a dictionary so that if the user passes in 25, and gets [1] back, they know thats a quarter
+// g continued) 
+// make new value for coinValues and pass that in
+
+// also what if they passed in 25
+// [1], but how do you know if it's quarters, dimes, nickles, pennies
+// You could solve this by using a 
 
 // Fixes:
 // 1. How do we use index values in a recursive function? 
 // 2. In the simple example of the doubledArray method we have the numArray being doubled into this new array. Can we combine .map() and recursion
 // to factor this code. That way, we would never need to instantiate/convert coinValues/coinCount
+// Is using i pure? How do we instantiate a variable one time, when the recursive call 
+
+// We're zeroing out coinValues -> using index(i)
+// How could we get the closure to collapse in such a way that we don't need to append each iterations coinCount
+
+// Define coincount at first level inside of function, but you declare let array = array, you'll pass it or - DONE
+// let coinCount = coinCount || [];
 
 // --------
 // Setup
@@ -57,10 +80,9 @@ console.log(coinCounter(789));
 
 // 19 Quarters, 2 dimes, 0 nickels, 4 pennies
 
-
-// Is using i pure? How do we instantiate a variable one time, when the recursive call 
-// We're zeroing out coinValues -> using index(i)
-// How could we get the closure to collapse in such a way that we don't need to append each iterations coinCount
-
-// Define coincount at first level inside of function, but you declare let array = array, you'll pass it or 
-// let coinCount = coinCount || [];
+// Pure Checklist:
+// 1. We always get the same output - DONE
+// 2. Always returns something - DONE
+// 3. Cannot rely on external variables - DONE
+// 4. Cannot rely on external state - DONE - not 100% sure
+// 5. No side effects - DONE
